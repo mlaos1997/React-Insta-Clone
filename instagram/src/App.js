13f129c666763c44e3baab5638d48121a3f827c2
Marcelo Laos <mlaos1997@gmail.com>
2019-03-20 22:01:54 -0400

    Created PostsPage Component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,40 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import dummyData from "./dummy-data";
-import SearchBar from "./components/SearchBar/SearchBar";
-import PostsContainer from "./components/PostsContainer/PostsContainer";
-
+import PostsPage from "./components/PostsContainer/PostsPage";
 class App extends Component {
-  state = {
-    posts: [],
-    filteredPosts: []
-  };
-
-  componentDidMount = () => {
-    this.setState({ posts: dummyData });
-  };
-
-  filterPosts = e => {
-    const filterPost = this.state.posts.filter(post => {
-      if (post.username.includes(e.target.value)) {
-        return post;
-      }
-    });
-    this.setState({ filteredPosts: filterPost });
-  };
-
   render() {
     return (
       <div className="App">
-        <SearchBar filterPosts={this.filterPosts} />
-        <PostsContainer
-          likes={this.state.posts.likes}
-          posts={
-            this.state.filteredPosts.length > 0
-              ? this.state.filteredPosts
-              : this.state.posts
-          }
-        />
+        <PostsPage />
       </div>
     );
   }
